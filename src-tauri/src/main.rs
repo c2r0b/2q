1f5258e3b0ec72d2834027d62a703977356b7d10
c2r0b <c2r0b@proton.me>
2023-06-14 17:45:21 +0200

    window bar

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{Runtime, Window, Manager};
 use async_graphql::{Schema, EmptySubscription};
 use async_graphql_warp::graphql;
 use warp::Filter;
@@ -22,6 +23,43 @@ use crate::query_root::QueryRoot;
 use crate::mutation_root::MutationRoot;
 
 type MySchema = Schema<QueryRoot, MutationRoot, EmptySubscription>;
+
+#[allow(dead_code)]
+pub enum ToolbarThickness {
+    Thick,
+    Medium,
+    Thin,
+}
+
+pub trait WindowExt {
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self);
+}
+
+impl<R: Runtime> WindowExt for Window<R> {
+    #[cfg(target_os = "macos")]
+    fn set_transparent_titlebar(&self) {
+        use cocoa::appkit::{NSWindow, NSWindowTitleVisibility};
+
+        unsafe {
+            let id = self.ns_window().unwrap() as cocoa::base::id;
+
+            id.setTitlebarAppearsTransparent_(cocoa::base::YES);
+
+            id.setTitleVisibility_(NSWindowTitleVisibility::NSWindowTitleHidden);
+            make_toolbar(id);
+        }
+    }
+}
+
+#[cfg(target_os = "macos")]
+unsafe fn make_toolbar(id: cocoa::base::id) {
+    use cocoa::appkit::{NSToolbar, NSWindow};
+
+    let new_toolbar = NSToolbar::alloc(id);
+    new_toolbar.init_();
+    id.setToolbar_(new_toolbar);
+}
 
 #[tokio::main]
 async fn main() {
@@ -57,6 +95,11 @@ async fn main() {
     });
   
     tauri::Builder::default()
+      .setup(|app| {
+        let win = app.get_window("main").unwrap();
+        win.set_transparent_titlebar();
+        Ok(())
+      })
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
--- src/layout/window/main/main.ts
+++ src/layout/window/main/main.ts
@@ -16,7 +16,8 @@ export class MainWindow extends StyledElement() {
 
   protected render() {
     return html`
-      <div class="flex h-full bg-gray-50 dark:bg-gray-900">
+      <div data-tauri-drag-region class="h-10 border-b border-b-gray-200 dark:border-b-gray-700 select-none flex justify-end fixed top-0 left-0 right-0 rounded-t-3xl"></div>
+      <div class="flex h-full bg-gray-50 dark:bg-gray-900 absolute top-10 bottom-0">
         <app-header></app-header>
         <side-menu
           @sectionChange="${this._handleSectionChange}"
